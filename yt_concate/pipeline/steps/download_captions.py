2486c7bcb8f92c0b72e29767dca85f072a654a06
joechien29 <76095375+joechien29@users.noreply.github.com>
2022-09-02 19:56:31 +0800

    Successfully search caption,  create found model, and add str and repr

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -13,22 +13,24 @@ class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         # download the package by:  pip install pytube
         start = time.time()
-        for url in data:
-            print('downloading caption for', url)
-            if utils.caption_file_exists((url)):
+        for yt in data:
+            print('downloading caption for', yt.id)
+            if utils.caption_file_exists(yt):
                 print('found existing caption file')
                 continue
             try:
-                source = YouTube(url)
+                source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
                 en_caption_convert_to_srt = (en_caption.generate_srt_captions())
             except (KeyError, AttributeError):
-                print('Error when downloading caption for', url)
+                print('Error when downloading caption for', yt.url)
                 continue
 
-            text_file = open(utils.get_caption_filepath(url), 'w', encoding='utf-8')
+            text_file = open(yt.caption_filepath, 'w', encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
             break
         end = time.time()
         print('-----took', end - start, 'second-----')
+
+        return data
